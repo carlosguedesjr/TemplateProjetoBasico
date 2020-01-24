8861adde33d622164c1ff5b7f16e3b4e62964cec
Guedes <carlosguedesjr@gmail.com>
2020-01-24 00:21:17 -0300

    versão inicial

---
+++ src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -0,0 +1,36 @@
+import { Usuario } from './models/usuario';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html'
+})
+export class CadastroComponent implements OnInit {
+
+  cadastroForm: FormGroup;
+  usuario: Usuario;
+  formResult: string = '';
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.cadastroForm = this.fb.group({
+      nome: ['', Validators.required],
+      cpf: [''],
+      email: ['', [Validators.required
+                 , Validators.email]],
+      senha: [''],
+      senhaConfirmacao: ['']
+    })
+  }
+
+  adicionarUsuario() {
+    if (this.cadastroForm.dirty && this.cadastroForm.valid) {
+      this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
+      this.formResult = JSON.stringify(this.cadastroForm.value);
+      debugger;
+    }
+  }
+
+}
