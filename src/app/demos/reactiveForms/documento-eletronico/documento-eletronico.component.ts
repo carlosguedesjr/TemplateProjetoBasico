a6128bcd731a61776f8adcb23644d2eb568c3b16
Guedes <carlosguedesjr@gmail.com>
2020-01-30 21:34:00 -0300

    estrutura de pasta de nota

---
+++ src/app/demos/reactiveForms/documento-eletronico/documento-eletronico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-documento-eletronico',
+  templateUrl: './documento-eletronico.component.html',
+  styles: []
+})
+export class DocumentoEletronicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
